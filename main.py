8a58fa00132ae3ea55d8e76f72d81ac86b47e503
GVatest <vasiliyganja@billing.net.kht.ru>
2022-06-19 17:51:46 +1000

    fix server error

--- main.py
+++ main.py
@@ -38,7 +38,11 @@ def quiz():
         resp = {
             "session": data["session"],
             "response": {},
-            "version": "1.0"
+            "version": "1.0",
+            "session_state": {
+                "question": "0",
+                "score": "0"
+            }
         }   
         if ("старт" in data["request"]["command"]):
             resp["response"] = {
@@ -52,143 +56,142 @@ def quiz():
                 "score": "0"
             }
             return json.dumps(resp)
-        if "state" in data.keys():
-            if data["state"]["session"]["question"] == "1":
-                resp["response"] = {
-                    "text": "Второй вопрос: В каком Doom первый раз запустили не на компьютере",
-                    "tts": "Второй вопрос: В каком Doom первый раз запустили не на компьютере",
-                    "end_session": False,
-                }
+        if data["state"]["session"]["question"] == "1":
+            resp["response"] = {
+                "text": "Второй вопрос: В каком Doom первый раз запустили не на компьютере",
+                "tts": "Второй вопрос: В каком Doom первый раз запустили не на компьютере",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "1923" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "2":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "1923" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "2":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "3":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "3":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "4":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "4":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "5":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "5":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "6":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "6":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "7":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "7":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "2",
-                    "score": srt(score)
-                }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "8":
-                resp["response"] = {
-                    "text": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "tts": "Третий вопрос: В каком году на рынок вышла фигма",
-                    "end_session": False,
-                }
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "2",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "8":
+            resp["response"] = {
+                "text": "Третий вопрос: В каком году на рынок вышла фигма",
+                "tts": "Третий вопрос: В каком году на рынок вышла фигма",
+                "end_session": False,
+            }
 
-                score = int(data["state"]["session"]["score"])
-                if "2015" in data["request"]["command"]:
-                    score += 1
-                resp["session_state"] = { 
-                    "question": "8",
-                    "score": srt(score)
+            score = int(data["state"]["session"]["score"])
+            if "2015" in data["request"]["command"]:
+                score += 1
+            resp["session_state"] = { 
+                "question": "8",
+                "score": srt(score)
+            }
+            return json.dumps(resp)
+        elif data["state"]["session"]["question"] == "8":
+            score = int(data["state"]["session"]["score"])
+            if score <= 5:
+                resp["response"] = {
+                    "text": "Вам подойдут все категории, кроме дизайна",
+                    "tts": "Вам подойдут все категории, кроме дизайна",
+                    "end_session": True,
                 }
-                return json.dumps(resp)
-            elif data["state"]["session"]["question"] == "8":
-                score = int(data["state"]["session"]["score"])
-                if score <= 5:
-                    resp["response"] = {
-                        "text": "Вам подойдут все категории, кроме дизайна",
-                        "tts": "Вам подойдут все категории, кроме дизайна",
-                        "end_session": True,
-                    }
-                else:
-                    resp["response"] = {
-                        "text": "Больше всего вам подойдет дизайн",
-                        "tts": "Больше всего вам подойдет дизайн",
-                        "end_session": True,
-                    }
-                score = int(data["state"]["session"]["score"])
-                return json.dumps(resp)
+            else:
+                resp["response"] = {
+                    "text": "Больше всего вам подойдет дизайн",
+                    "tts": "Больше всего вам подойдет дизайн",
+                    "end_session": True,
+                }
+            score = int(data["state"]["session"]["score"])
+            return json.dumps(resp)
 
         resp["response"] = {
                 "text": "Ошибка",
