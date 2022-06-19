a3e14df8db30b65a9713f8ea9839eff556e09687
GVatest <vasiliyganja@billing.net.kht.ru>
2022-06-19 16:44:55 +1000

    fix server erro

--- main.py
+++ main.py
@@ -7,28 +7,31 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    data = json.loads(request.data)
-    resp = {
-        "session": data["session"],
-        "response": {
-            "text": "Привет вездекодерам!",
-            "tts": "Привет вездекодерам!",
-            "end_session": true
-        },
-    }
-    if ("ThereIsNoInfoRoma" in data["request"]["command"] and "Вездекод" in data["request"]["command"]):
+    if request.method == "POST":
+        data = json.loads(request.data)
+        resp = {
+            "session": data["session"],
+            "response": {
+                "text": "Привет вездекодерам!",
+                "tts": "Привет вездекодерам!",
+                "end_session": true
+            },
+        }
+        if ("ThereIsNoInfoRoma" in data["request"]["command"] and "Вездекод" in data["request"]["command"]):
+            resp["response"] = {
+                "text": "Привет вездекодерам!",
+                "tts": "Привет вездекодерам!",
+                "end_session": true
+            },
+            return resp
+        
         resp["response"] = {
-            "text": "Привет вездекодерам!",
-            "tts": "Привет вездекодерам!",
-            "end_session": true
+                "text": "Сегодня без привета",
+                "tts": "Сегодня без привета",
+                "end_session": true
         },
         return resp
-    
-    resp["response"] = {
-            "text": "Сегодня без привета",
-            "tts": "Сегодня без привета",
-            "end_session": true
-    },
-    return r
+    else:
+        return "Hello"
     
 
